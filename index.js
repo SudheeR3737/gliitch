ba34e9ce900a61b2fe4a64ed8fcd4c155b17c46f
SudheeR3737 <83955438+SudheeR3737@users.noreply.github.com>
2022-10-24 11:50:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    Node: require("./src/Node"),
+    Player: require("./src/Player"),
+    Glitch: require("./src/Glitch"),
+    Spotify : require("./src/platform/Spotify"),
+    Deezer : require("./src/platform/Deezer"),
+    AppleMusic : require("./src/platform/AppleMusic"),
+    Queue : require("./src/guild/Queue"),
+    Filter : require("./src/guild/Filter"),
+    Track: require("./src/guild/Track"),
+    GlitchTrack : require("./src/guild/GlitchTrack")
+    
+    
+
+}
